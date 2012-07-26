dd54172d19151ffd3a68ac1bedd8cd257f059e41
Ross Walker <jolliffeross@gmail.com>
2012-07-26 16:50:35 +0100

    Added Message class to handle messages to the player

---
+++ src/me/rosswalker/serverstats/Messages.java
@@ -0,0 +1,29 @@
+package me.rosswalker.serverstats;
+
+import org.bukkit.ChatColor;
+
+/* @name Messages
+ * @author DicaxDorcas
+ * @description Class to handle getters for player messages.
+ */
+
+
+public class Messages {
+
+	ServerStats plugin;
+	
+	/* @name getHeader()
+	 * @description Returns a string containing the plugin header.
+	 */
+	String getHeader(){
+		return ChatColor.AQUA +"[" + plugin.getName() + "]" + ChatColor.WHITE;
+	}
+	
+	/* @name getHeader()
+	 * @param1 integer
+	 * @description Returns a string containing the plugin header.
+	 */
+	String getError(int number){
+	    return "";
+	}
+}

42d6f5674f51a7958081ea27f58cfb9b100fbbb4
Ross Walker <jolliffeross@gmail.com>
2012-07-26 16:13:12 +0100

    Removed depreciated listener class, tidied up the main class a bit.

--- src/me/rosswalker/serverstats/ServerStats.java
+++ src/me/rosswalker/serverstats/ServerStats.java
@@ -27,7 +27,7 @@ public class ServerStats extends JavaPlugin {
 			if (cmd.getName().equalsIgnoreCase("ss")) {
 				
 				//Stats Command
-				else if (args[0].equals("stats")) {
+				else if (args[0].equalsIgnoreCase("stats")) {
 					if (player.hasPermission("stats.stats")) {
 						int totalPlayers = (getServer().getOfflinePlayers().length + getServer().getOnlinePlayers().length);
 						int onlinePlayers = getServer().getOnlinePlayers().length;
@@ -58,7 +58,7 @@ public class ServerStats extends JavaPlugin {
 				
 				//Player Command
 				
-				else if (param1.equals("player")) {
+				else if (args[0].equalsIgnoreCase("player")) {
 					try {
 						if (args.length < 2) {
 							if (player.hasPermission("stats.player")) {
@@ -95,204 +95,20 @@ public class ServerStats extends JavaPlugin {
 						player.sendMessage(ChatColor.RED + "An error occurred, please try again.");
 					}
 				}
-				
-				
-				
-				//Reset Command
-				
-				else if (param1.equals("reset")) {
-					if (player.hasPermission("stats.reset")) {
-						File playerLogger = new File(getDataFolder() + "/players.txt");
-						if (playerLogger.exists()) {
-							try {
-								player.sendMessage(ChatColor.BLUE + "----------ServerStats----------");
-								playerLogger.delete();
-								System.out.println("'players.txt' file deleted. Creating new file...");
-								player.sendMessage(ChatColor.GRAY + "ServerStats data emptied. Generating new file...");
-								playerLogger.createNewFile();
-								player.sendMessage(ChatColor.GRAY + "File created.");
-								System.out.println("'players.txt' created.");
-								player.sendMessage(ChatColor.BLUE + "--------------------------------");
-							}
-							catch (IOException e)
-							{
-								player.sendMessage(ChatColor.RED + "An error occured, see console for more info.");
-								player.sendMessage(ChatColor.BLUE + "--------------------------------");
-								System.out.println("[ServerStats] Unable to create 'players.txt' file in " + playerLogger.getPath() + "/" + playerLogger.getName());
-								e.printStackTrace();
-							}
-						}
-						else {
-							try {
-								player.sendMessage(ChatColor.BLUE + "----------ServerStats----------");
-								player.sendMessage(ChatColor.GRAY + "ServerStats has not yet created data files. Creating now...");
-								System.out.println("[ServerStats] Creating 'players.txt' file...");
-								playerLogger.createNewFile();
-								player.sendMessage(ChatColor.GRAY + "File created.");
-								System.out.println("File created.");
-								player.sendMessage(ChatColor.BLUE + "--------------------------------");
-							}
-							catch (IOException e)
-							{
-								player.sendMessage(ChatColor.RED + "An error occured, see console for more info.");
-								player.sendMessage(ChatColor.BLUE + "--------------------------------");
-								System.out.println("[ServerStats] Unable to create 'players.txt' file in " + playerLogger.getPath() + "/" + playerLogger.getName());
-								e.printStackTrace();
-							}
-						}
-					}
-				}
-				
-				//Unknown Command
-				
-				else {
-					if (player.hasPermission("stats.unknown")) {
-						player.sendMessage(ChatColor.BLUE + "----------ServerStats----------");
-						player.sendMessage(ChatColor.RED + "  Command not recognized.");
-						player.sendMessage(ChatColor.GRAY + "  Type " + ChatColor.DARK_GRAY + "/ss help " + ChatColor.GRAY + "for a list of commands.");
-						player.sendMessage(ChatColor.BLUE + "--------------------------------");
-						return true;
-					}
-					else {
-						player.sendMessage(ChatColor.RED + "You don't have permission to use this command.");
-					}
-				}
-			}
-		}
-		else {
+		} else {
 			System.out.println("[ServerStats] The console does not have access to this plugin.");
 		}
 		return false;
-	}
+		}
 
-	Logger log;
-	
-	/**
-	 * Rage command with second parameter.
-	 * @param player
-	 * @param damage
-	 * @return
-	 */
-	
-	public boolean rage(Player player, int damage)
-	{
-		float explosionPower = damage;
-		int lightningStrikes = damage;
-		World world = player.getWorld();
-		Location location = player.getLocation();
-		for (int i=0;i<lightningStrikes;i++) {
-			world.strikeLightning(location);
-			try {
-				Thread.currentThread();
-				Thread.sleep(250);
-			} catch (InterruptedException e) {
-				e.printStackTrace();
-			}
-		}
-		world.createExplosion(location, explosionPower);
-		return true;
-	}
-	
-	/**
-	 * Rage command with no parameters.
-	 * @param player
-	 * @return
-	 */
-	
-	public boolean rageWithTarget(Player player)
-	{
-		Location location = player.getTargetBlock(null, 600).getLocation();
-		for (int i=0;i<5;i++)
-		{
-			player.getWorld().strikeLightning(location);
-			try {
-				Thread.currentThread();
-				Thread.sleep(250);
-			} catch (InterruptedException e) {
-				e.printStackTrace();
-			}
-		}
-		player.getWorld().createExplosion(location, 5F);
-		return true;
-	}
-	
-	/**
-	 * Lock down server, and kick all players without "stats.lockdown.exempt" permission.
-	 * @param sender
-	 */
-	
-	private void lockdown(Player sender)
-	{
-		System.out.println("[ServerStats] " + sender.getName() + " has put the server into lockdown mode.");
-		System.out.println("[ServerStats] All players will now be kicked.");
-		for(Player player : getServer().getOnlinePlayers())
-		{
-			if(player.hasPermission("stats.lockdown.exempt") && player != sender)
-			{
-				player.sendMessage(ChatColor.BLUE + "----------ServerStats------------");
-				player.sendMessage(ChatColor.RED + "The server is currently in lockdown mode, we are");
-				player.sendMessage(ChatColor.RED + "not accepting any connections of non-exempted players.");
-				player.sendMessage(ChatColor.BLUE + "----------------------------------");
-			}
-			else if(player.hasPermission("stats.lockdown.exempt") && player == sender)
-			{
-				//Do nothing
-			}
-			else {
-				player.kickPlayer("The server is currently in lockdown mode. Come back later.");
-			}
-		}
-	}
-	
-	/**
-	 * Rage command without 2nd parameter.
-	 * @param player
-	 * @return
-	 */
-	
-	public boolean rage(Player player)
-	{
-		return rage(player, 5);
-	}
-	
-	/**
-	 * Return whether lockdown is true or false.
-	 * @return on/off
-	 */
-	
-	public String lockdownStatus()
-	{
-		if(SSListener.lockdown = true)
-		{
-			return "on";
-		}
-		else {
-			return "off";
-		}
-	}
-	
+		
     public void onEnable() {
     	new SSListener(this);
-        if (!new File(getDataFolder().toString()).exists()) {
-            new File(getDataFolder().toString()).mkdir();
-          }
-    	
-    	File file = new File(getDataFolder() + "/players.txt"); 
-    	
-    	if (!file.exists()) {
-    		try {
-    			file.createNewFile();
-    		} catch (IOException e) {
-    			System.out.println("[ServerStats] Unable to create 'players.txt' file in " + file.getPath() + "/" + file.getName());
-    		}
-    	}
-    	
-		 log = this.getLogger();
-		 log.info("Plugin enabled.");
+    	this.getLogger().info("Plugin enabled.");
 	}
 	 
 	public void onDisable(){ 
-		 log.info("Plugin disabled.");
+		this.getLogger().info("Plugin disabled.");
 	}	
 
 }
